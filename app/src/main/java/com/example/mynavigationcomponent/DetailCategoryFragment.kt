c07c1051479f158a6862262069e147a5b024a201
yusuf <yusufnew060@gmail.com>
2025-02-03 09:17:35 +0700

    Menambahkan button di detail kategori

--- app/src/main/java/com/example/mynavigationcomponent/DetailCategoryFragment.kt
+++ app/src/main/java/com/example/mynavigationcomponent/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.mynavigationcomponent.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -35,6 +36,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescrpition.text = "Stok : $dataDescription"
+
+        binding.btnHome.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
